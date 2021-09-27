719074559942d075a4bf5ddbfed607317857abd3
VincentChen6345 <vincent.chen6345@hotmail.com>
2021-09-27 13:56:51 +1000

    refactored

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 "strict mode";
-const btn1 = document.querySelector(".btn-1");
-const btn2 = document.querySelector(".btn-2");
-const btn3 = document.querySelector(".btn-3");
+const btn = document.querySelectorAll(".btn");
 const modal = document.querySelectorAll(".modal");
-const modal1 = document.querySelector(".modal-1");
-const modal2 = document.querySelector(".modal-2");
-const modal3 = document.querySelector(".modal-3");
 const overlay = document.querySelector(".overlay");
-const closeBTN1 = document.querySelector(".close-1");
-const closeBTN2 = document.querySelector(".close-2");
-const closeBTN3 = document.querySelector(".close-3");
+const closeBTN = document.querySelectorAll(".close-modal");
+
+////////////////////
 
 const closeModal = function () {
-  modal1.classList.add("hidden");
-  modal2.classList.add("hidden");
-  modal3.classList.add("hidden");
+  for (let i = 0; i < modal.length; i++) {
+    modal[i].classList.add("hidden");
+  }
   overlay.classList.add("hidden");
 };
 ////////////////////
 //show modal buttons
-btn1.addEventListener("click", function () {
-  modal1.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-});
 
-btn2.addEventListener("click", function () {
-  modal2.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-});
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    modal[i].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
 
-btn3.addEventListener("click", function () {
-  modal3.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-});
+////////////////////
+//close modal button
+for (let i = 0; i < closeBTN.length; i++) {
+  closeBTN[i].addEventListener("click", closeModal);
+}
 
-closeBTN1.addEventListener("click", closeModal);
-closeBTN2.addEventListener("click", closeModal);
-closeBTN3.addEventListener("click", closeModal);
 ///////////////
 ///////////////
 //////escape key function
@@ -49,6 +40,3 @@ document.addEventListener("keydown", function (e) {
 //////////////
 /////overlay click function
 overlay.addEventListener("click", closeModal);
-
-///////////////
-/////
